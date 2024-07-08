4ff77b21a6cbf8cc254a846b174e4c593e71c67c
wilfriid <kouameezekiel29@gmail.com>
2024-07-08 18:04:57 +0000

    ProjetMaison

---
+++ main.js
@@ -0,0 +1,132 @@
+// part
+
+  
+  document.getElementById("openPopup").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("popup").style.display = "block";
+  });
+  
+  document.querySelector(".close").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+  });
+  // 
+  function changerTexte1() {
+    var bouton = document.getElementById("bouton-plus-moins1");
+    var texteCache = document.getElementById("texte-cache1");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte2() {
+    var bouton = document.getElementById("bouton-plus-moins2");
+    var texteCache = document.getElementById("texte-cache2");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte3() {
+    var bouton = document.getElementById("bouton-plus-moins3");
+    var texteCache = document.getElementById("texte-cache3");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte4() {
+    var bouton = document.getElementById("bouton-plus-moins4");
+    var texteCache = document.getElementById("texte-cache4");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte5() {
+    var bouton = document.getElementById("bouton-plus-moins5");
+    var texteCache = document.getElementById("texte-cache5");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte6() {
+    var bouton = document.getElementById("bouton-plus-moins6");
+    var texteCache = document.getElementById("texte-cache6");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte7() {
+    var bouton = document.getElementById("bouton-plus-moins7");
+    var texteCache = document.getElementById("texte-cache7");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte8() {
+    var bouton = document.getElementById("bouton-plus-moins8");
+    var texteCache = document.getElementById("texte-cache8");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte9() {
+    var bouton = document.getElementById("bouton-plus-moins9");
+    var texteCache = document.getElementById("texte-cache9");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+  // 
+  function changerTexte10() {
+    var bouton = document.getElementById("bouton-plus-moins10");
+    var texteCache = document.getElementById("texte-cache10");
+    if (bouton.textContent === "+") {
+      bouton.textContent = "-";
+      texteCache.style.display = "block";
+    } else {
+      bouton.textContent = "+";
+      texteCache.style.display = "none";
+    }
+  }
+
